e34a409dab8c942c7c9911d424a134ecf6a0bab6
milomacphail <milomacphail@gmail.com>
2019-06-01 17:57:08 -0400

    Initialized packages and basic code to open site

---
+++ SeleniumSandbox/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumSandbox
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //create reference for browser
+            IWebDriver driver = new ChromeDriver();
+
+            //Navigate to Google.com
+            driver.Navigate().GoToUrl("http://www.google.com");
+
+            //type a value into Google
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys("careerdevs Providence");
+             
+
+        }
+    }
+}
